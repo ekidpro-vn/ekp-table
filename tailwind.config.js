9ae0470878e075b78106f2fd12831600d06d39f3
Nguyen Duc <ducnh1@egroup.vn>
2021-02-05 20:21:44 +0700

    Thêm tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: ['./src/**/*.js', './src/**/*.jsx', './src/**/*.ts', './src/**/*.tsx'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    // screens: {
+    //   xs: '320px',
+    //   sm: '640px',
+    //   md: '768px',
+    //   lg: '1024px',
+    //   xl: '1280px',
+    //   '2xl': '1536px',
+    // },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
